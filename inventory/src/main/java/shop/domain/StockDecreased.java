1fce677b4361283d06fb157630c023461d2ce68b
Sung-Yeol Yun <existmaster@gmail.com>
2025-06-28 17:13:54 +0900

    commit

---
+++ inventory/src/main/java/shop/domain/StockDecreased.java
@@ -0,0 +1,26 @@
+package shop.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import shop.domain.*;
+import shop.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class StockDecreased extends AbstractEvent {
+
+    private Long id;
+    private String name;
+    private Integer stock;
+
+    public StockDecreased(Inventory aggregate) {
+        super(aggregate);
+    }
+
+    public StockDecreased() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
